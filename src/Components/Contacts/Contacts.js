29ca67944aa891e7dc84fc8e86f3dbaada656076
Viktor <67584100+Viktor85-prog@users.noreply.github.com>
2020-11-05 19:21:12 +0300

    hook useContacts

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -1,8 +1,54 @@
 import React from "react";
 import styles from "./Contacts.module.css";
+import { useState, useEffect } from "react";
+
+const useContacts = () => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch("https://randomuser.me/api/?results=200");
+        const { results, error } = await response.json();
+        if (error) {
+          throw new Error(error);
+        }
+        setData(results);
+        setIsError(false);
+      } catch (e) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getContacts();
+  }, []);
+  return {
+    isLoading,
+    isError,
+    data,
+  };
+};
 
 let Contacts = () => {
-  return <div>Contacts</div>;
+  const contacts = useContacts();
+  if (contacts.isLoading) {
+    return <div> ... loading</div>;
+  }
+
+  if (contacts.isError) {
+    return <div> ...error</div>;
+  }
+
+  return (
+    <div>
+      Contacts
+      {contacts.data[0].name.first}
+    </div>
+  );
 };
 
 export default Contacts;
